b33a5bf6bf04661a31fbd542242214e8edb8fee7
dhruv-barapatre <barapatredhruv28012006@gmail.com>
2024-07-31 15:41:12 +0530

    Okay All

--- my-app/src/Page/Women.jsx
+++ my-app/src/Page/Women.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 function Women() {
   return (
-    <div>Women</div>
+    <>
+    
+      <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>On-Trend Clothes For Teenage Girls</h2>
+          <p>
+            When you're looking for perfectly stylish teenage clothing, our selection of cute clothes for girls and women offers just what you need for all seasons. Put together a variety of chic, comfortable outfits to stay ready for whatever the day throws your way.
+          </p>
+          <p>
+            Teen girls' fashion never stands still. Keep up and stay on-trend with the range of cute clothes for teenage girls and young women at Aéropostale. From stylish denim and the coziest activewear to fun dresses and trendy jumpsuits, find great teen girl clothing options to dress your best for work, school and play.
+          </p>
+          <h3>Stay Cool In Teen Girls' Fashion Staples For Warm Weather</h3>
+          <p>
+            During the warm spring and summer months, fill your closet with girls' clothes designed to let you shine. Bask in the sun to your heart's content with a cropped cami and high-rise denim shorts to pair with your sandals or fashion sneakers. When heading out for the evening, show off your fun, flirty side in a flouncy mini dress or matching skirt and top set. For more of an edgy, rocker vibe, you can't go wrong with distressed skater shorts featuring whiskering and fading in all the right places to pair with your favorite tank tops.
+          </p>
+          <h3>Bundle Up With Teen Clothing For Colder Weather</h3>
+          <p>
+            Combat the cold without sacrificing style in cute clothes for girls that serve as the perfect standalone or layering pieces. Start from the bottom up with the right girls' jeans, teen leggings and jeggings available in a range of cuts and fits to show off your true style. Then, add a variety of tops like long-sleeve button downs or graphic tees to wear under your jacket or hoodie.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <h3>Must-Have Clothes For Teenage Girls</h3>
+          <p>
+            You have a style that's all your own, so show it off with must-have cute clothes for teens from Aéropostale. Treat yourself to some comfortable girls' clothing, accessories and shoes, including these popular essentials.
+          </p>
+          <ul>
+            <li>Teen Girl Tops: From pullover hoodies to graphic tees, find all the teen <a href="#">teen girls' tops</a> you need to create your special style.</li>
+            <li>Teen Girl Bottoms: Whether your outfit calls for jeans, shorts, skirts, jeggings or joggers, our selection of <a href="#">women's and girls' bottoms</a> has you covered.</li>
+            <li>Teen Dresses: Shop our <a href="#">dresses and rompers</a> for fun and casual styles suitable for a specific occasion or everyday wear.</li>
+            <li>Teen Girl Jackets and Coats: Our <a href="#">girls' jackets collection</a> features outerwear of all types, including cute pullover jackets, hooded parkas and puffer vests.</li>
+            <li>Teen Girl Extended Sizes: Cute girls' fashion comes in every size at Aéropostale, your go-to teen clothing store for plus sizes.</li>
+          </ul>
+          <h3>Shop Cute Clothes For Teenage Girls And Women</h3>
+          <p>
+            Girls go through clothes fast, especially during the active teenage years. From the latest looks and popular trends to standout designs for creating a distinctive look, the Aéropostale range of cute clothes for teens and women contains everything you need. Whether you're heading back to school, getting ready for winter break, or just interested in a makeover, our line of teen clothing won't disappoint. Browse our selection to start creating your young woman's wardrobe with distinctive style. Finish your look by pairing our latest clothing for teenage girls with popular <a href="#">girls' accessories</a> like sunglasses, scrunchies and hats.
+          </p>
+        </div>
+      </div>
+    </div>
+    
+    </>
   )
 }
 
