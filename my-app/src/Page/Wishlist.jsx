b3d9c14efd664f5c35311e6b7995fcf7b7cdce68
niksnimje <niksnimje4@gmail.com>
2024-08-04 14:38:37 +0530

    All Set

--- my-app/src/Page/Wishlist.jsx
+++ my-app/src/Page/Wishlist.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Wishlist() {
   return (
-    <div>Wishlist</div>
+    <>
+      <div className="container text-center pt-5">
+          <h4>My Wishlist</h4>
+          <p>If you are not signed in, your wishlist is only available on this device and will expire at the end of the session.</p> <br />
+          <p><Link to={'/account'} className='text-primary'>Sign In</Link> or  <Link to={'/account'} className='text-primary'>Create New Account</Link></p>
+          <p>Your wishlist is empty.</p> <br />
+          <button className='btn btn-primary text-light'><Link to={"/"}>Start Adding Items</Link></button> <br /><br /><br />
+      </div>
+    </>
   )
 }
 
