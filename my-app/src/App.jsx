3991bd4530c206f50bb1cac958c4ebacdcf78a12
niksnimje <niksnimje4@gmail.com>
2024-08-07 16:07:59 +0530

    All Firebase okay

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import NavbarCompo from './Components/NavbarCompo'
 import AllRouters from './Routers/AllRouters'
--- my-app/src/Page/CreateAccount.jsx
+++ my-app/src/Page/CreateAccount.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
-import { auth, provider } from '../FireBase/Firebas';
+import { auth, provider, provider2 } from '../FireBase/Firebas';
 import { signInWithPopup } from 'firebase/auth';
 import GoogleButton from 'react-google-button'
+import FacebookLogin from 'react-facebook-login';
 
 
 function CreateAccount() {
     
     const hendelogin=()=>{
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, provider, provider2)
         .then((res)=>console.log(res))
         .catch((err)=>console.log(err))
       }
   return (
     <>
     <div>
+        <div className="container">
+            <div className="row">
+                <div className="col-5">
+                    <form action="">
+                        <input type="text" placeholder='First Name' />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
         <GoogleButton onClick={hendelogin}/>
+        <FacebookLogin onClick={hendelogin}/>
     </div>
     </>
 
