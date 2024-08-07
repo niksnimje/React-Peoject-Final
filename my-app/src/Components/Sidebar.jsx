bc4571632210d5605e875a4e94fbd7e226ac33a3
niksnimje <niksnimje4@gmail.com>
2024-08-07 16:53:46 +0530

    All Ok fetch

--- my-app/src/Components/Sidebar.jsx
+++ my-app/src/Components/Sidebar.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 
-function Sidebar() {
+function Sidebar({ onFilterChange }) {
+  const handleCheckboxChange = (event) => {
+    const { value, checked } = event.target;
+    onFilterChange(value, checked); // Pass the color and checked status to parent component
+  };
+
   return (
     <div className="sidebar d-none d-sm-none d-md-block d-lg-block">
       <div className="sidebar-section">
         <h6>Category</h6>
         <ul>
-          <li><input type="checkbox" /> Top</li>
-          <li><input type="checkbox" /> Bottoms</li>
-          {/* <li><input type="checkbox" /> Dresses & Jumpsuits</li> */}
-          <li><input type="checkbox" /> Matching Sets</li>
+          <li><input type="checkbox" value="Top" onChange={handleCheckboxChange} /> Top</li>
+          <li><input type="checkbox" value="Bottoms" onChange={handleCheckboxChange} /> Bottoms</li>
+          <li><input type="checkbox" value="Matching Sets" onChange={handleCheckboxChange} /> Matching Sets</li>
         </ul>
       </div>
       <div className="sidebar-section">
         <h6>Color</h6>
         <ul className="color-list">
-          <li className="color-option"><input type="checkbox" /> <span className="color-box black"></span> Black</li>
-          <li className="color-option"><input type="checkbox" /> <span className="color-box blue"></span> Blue</li>
-          <li className="color-option"><input type="checkbox" /> <span className="color-box brown"></span> Brown</li>
-          <li className="color-option"><input type="checkbox" /> <span className="color-box green"></span> Green</li>
-          <li className="color-option"><input type="checkbox" /> <span className="color-box white"></span> White</li>
+          <li className="color-option"><input type="checkbox" value="Black" onChange={handleCheckboxChange} /> <span className="color-box black"></span> Black</li>
+          <li className="color-option"><input type="checkbox" value="Blue" onChange={handleCheckboxChange} /> <span className="color-box blue"></span> Blue</li>
+          <li className="color-option"><input type="checkbox" value="Brown" onChange={handleCheckboxChange} /> <span className="color-box brown"></span> Brown</li>
+          <li className="color-option"><input type="checkbox" value="Green" onChange={handleCheckboxChange} /> <span className="color-box green"></span> Green</li>
+          <li className="color-option"><input type="checkbox" value="White" onChange={handleCheckboxChange} /> <span className="color-box white"></span> White</li>
         </ul>
       </div>
-     
     </div>
   );
 }
--- my-app/src/Page/Women.jsx
+++ my-app/src/Page/Women.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom'; // Highlighted: useLocation for capturing query params
+import { Link, useLocation } from 'react-router-dom';
 import Sidebar from '../Components/Sidebar';
 import './Css/Women.css';
 
@@ -9,11 +9,12 @@ function Women() {
   const [page, setpage] = useState(1);
   const [order, setorder] = useState(null);
   const [selectcategory, setcategory] = useState(null);
-  const location = useLocation(); // Highlighted: useLocation hook
+  const [selectedColors, setSelectedColors] = useState([]);
+  const location = useLocation();
 
   const WomensProduct = () => {
-    const params = new URLSearchParams(location.search); // Highlighted: Parse URL search params
-    const searchQuery = params.get('q'); // Highlighted: Get the search query from URL
+    const params = new URLSearchParams(location.search);
+    const searchQuery = params.get('q');
 
     axios
       .get('http://localhost:3000/women-product', {
@@ -23,7 +24,8 @@ function Women() {
           category: selectcategory || undefined,
           _sort: "price",
           _order: order,
-          q: searchQuery || undefined, // Highlighted: Include search query in params
+          q: searchQuery || undefined,
+          colors: selectedColors.length > 0 ? selectedColors.join(',') : undefined
         },
       })
       .then((res) => setwomendata(res.data))
@@ -32,14 +34,24 @@ function Women() {
 
   useEffect(() => {
     WomensProduct();
-  }, [page, order, selectcategory, location.search]); // Highlighted: Add location.search as a dependency
+  }, [page, order, selectcategory, selectedColors, location.search]);
+
+  const handleFilterChange = (color, isChecked) => {
+    setSelectedColors((prevColors) => {
+      if (isChecked) {
+        return [...prevColors, color];
+      } else {
+        return prevColors.filter((c) => c !== color);
+      }
+    });
+  };
 
   return (
     <>
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 col-md-3">
-            <Sidebar />
+            <Sidebar onFilterChange={handleFilterChange} />
           </div>
           
           <div className="col-12 col-md-9 offset-md-3">
@@ -61,7 +73,7 @@ function Women() {
               </div>
               {womendata.map((el) => (
                 <div key={el.id} className="pt-5 col-12 col-sm-6 col-md-4 col-lg-3 mb-4 text-center">
-                  <Link to={`/description/${el.id}`}> {/* Fixed template string syntax */}
+                  <Link to={`/description/${el.id}`}>
                     <img src={el.img} alt={el.title} className="img-fluid" />
                   </Link>
                   <h5>{el.title}</h5>
@@ -70,7 +82,7 @@ function Women() {
               ))}
             </div>
             <div className="btn-class text-center">
-              <button className='btn btn-primary' disabled={page === 0} onClick={() => setpage(page - 1)}>Prev</button>
+              <button className='btn btn-primary' disabled={page === 1} onClick={() => setpage(page - 1)}>Prev</button>
               <span>{page}</span>
               <button className='btn btn-primary' disabled={womendata.length < 10} onClick={() => setpage(page + 1)}>Next</button>
             </div>
